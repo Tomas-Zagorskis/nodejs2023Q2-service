ccf3385f95ade88f4b4678314bf68f52e26bfaf8
Tomas Zagorskis <tomzagos@gmail.com>
2023-07-29 21:13:15 +0300

    feat: get dependencies from other module

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { FavoritesService } from './favorites.service';
+import { AlbumModule } from 'src/album/album.module';
+import { ArtistModule } from 'src/artist/artist.module';
+import { TrackModule } from 'src/track/track.module';
 import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
 
 @Module({
+  imports: [TrackModule, AlbumModule, ArtistModule],
   controllers: [FavoritesController],
-  providers: [FavoritesService]
+  providers: [FavoritesService],
 })
 export class FavoritesModule {}
