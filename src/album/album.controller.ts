a368c3348e674053e41fb7c21f90c40b8ad25ce9
Tomas Zagorskis <tomzagos@gmail.com>
2023-07-29 18:39:20 +0300

    feat: implement album routes

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
@@ -8,7 +19,7 @@ export class AlbumController {
   constructor(private readonly albumService: AlbumService) {}
 
   @Post()
-  create(@Body() createAlbumDto: CreateAlbumDto) {
+  create(@Body(new ValidationPipe()) createAlbumDto: CreateAlbumDto) {
     return this.albumService.create(createAlbumDto);
   }
 
@@ -18,17 +29,21 @@ export class AlbumController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.albumService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-    return this.albumService.update(+id, updateAlbumDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body(new ValidationPipe()) updateAlbumDto: UpdateAlbumDto,
+  ) {
+    return this.albumService.update(id, updateAlbumDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.albumService.remove(+id);
+  @HttpCode(204)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    this.albumService.remove(id);
   }
 }
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from './entities/album.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class AlbumService {
+  private albums = new Map<string, Album>();
+
   create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+    const newAlbum: Album = {
+      id: uuid(),
+      ...createAlbumDto,
+    };
+    this.albums.set(newAlbum.id, newAlbum);
+    return newAlbum;
   }
 
   findAll() {
-    return `This action returns all album`;
+    const albums = Array.from(this.albums.values());
+    return albums;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  findOne(id: string) {
+    const album = this.albums.get(id);
+    if (!album) throw new NotFoundException('Album not found');
+    return album;
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    const album = this.findOne(id);
+
+    const updAlbum: Album = {
+      ...album,
+      ...updateAlbumDto,
+    };
+
+    this.albums.set(id, updAlbum);
+    return updAlbum;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  remove(id: string) {
+    this.findOne(id);
+    this.albums.delete(id);
   }
 }
