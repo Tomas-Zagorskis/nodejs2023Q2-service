ac68a875a8a6adeb015e389adf18700bbdcc823a
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-07 13:50:00 +0300

    feat: implement favorites with typeorm database

--- src/favorites/entities/favorites.entity.ts
+++ src/favorites/entities/favorites.entity.ts
@@ -1,5 +1,22 @@
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Track } from 'src/track/entities/track.entity';
+import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Favorites {
-  artists: string[];
-  albums: string[];
-  tracks: string[];
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToMany(() => Artist, { onDelete: 'SET NULL' })
+  @JoinTable()
+  artists: Artist[];
+
+  @ManyToMany(() => Album, { onDelete: 'SET NULL' })
+  @JoinTable()
+  albums: Album[];
+
+  @ManyToMany(() => Track, { onDelete: 'SET NULL' })
+  @JoinTable()
+  tracks: Track[];
 }
