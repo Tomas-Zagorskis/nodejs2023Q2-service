b022a19fb2bddfb663b828102a6be0ad5783e9f6
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-10 22:50:21 +0300

    feat: add migrations to create db entities

---
+++ src/config/typeOrm.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.getOrThrow('LOCAL_HOST'),
+  port: +configService.getOrThrow<number>('POSTGRES_PORT'),
+  username: configService.getOrThrow('POSTGRES_USER'),
+  password: configService.getOrThrow('POSTGRES_PASSWORD'),
+  database: configService.getOrThrow('POSTGRES_DB'),
+  migrations: ['./src/migrations/**'],
+  entities: ['./dist/*/entities/*.ts'],
+});
