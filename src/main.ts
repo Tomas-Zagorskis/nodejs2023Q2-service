5454b1313951313ab77a3462e47f929e5c28a9bb
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-13 11:52:32 +0300

    feat: add http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './http-exception/http-exception.filter';
 
 const PORT = +process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Home Music Library')
